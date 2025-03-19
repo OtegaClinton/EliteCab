a7af6247f63a9830c3e8349ff2f50584ebd2a439
Babaurch <madukwes@gmail.com>
2025-03-19 16:53:35 +0100

    Updated rideRoute and chatRoute

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 require("./config/db"); 
-const userRoutes = require("./routes/userRoute"); 
+const userRoutes = require("./routes/userRoute");
+const chatRoutes = require("./routes/chatRoute");
+const rideRoutes = require("./routes/rideRoute"); 
 
 const PORT = process.env.PORT || 2025;
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/v1", userRoutes);
+app.use("/api/v1", userRoutes, chatRoutes, rideRoutes);
 
 app.listen(PORT, () => {
     console.log(`🚀 Server is listening on PORT: ${PORT}`);
